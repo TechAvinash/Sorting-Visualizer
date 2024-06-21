daf814716eb54a1185a0a9bd9fa7e5d3fe3c192d
TechAvinash <149116081+TechAvinash@users.noreply.github.com>
2024-06-21 12:04:54 +0530

    Add files via upload

---
+++ AlgoCode/intrst.py
@@ -0,0 +1,7 @@
+a=int(input("Enter your amount:"))
+b=int(input("Enter Time Period in years:"))
+rate=5
+si=(a*rate*b)/100
+print("Interest that you need to pay is:",si)
+total=si+a
+print("Amount that you need to pay is - ",total)
